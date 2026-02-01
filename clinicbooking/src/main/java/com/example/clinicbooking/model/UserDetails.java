fa355e6fdc025cfbab0f21ab4c18ed500dedd8f4
Shru123456789 <167177204+Shru123456789@users.noreply.github.com>
2026-02-01 10:27:25 +0530

    Update UserDetails.java

--- clinicbooking/src/main/java/com/example/clinicbooking/model/UserDetails.java
+++ clinicbooking/src/main/java/com/example/clinicbooking/model/UserDetails.java
@@ -1,42 +1,3 @@
-// package com.example.clinicbooking.model;
-
-// import java.time.LocalDate;
-
-// public class UserDetails {
-
-//     private String name;
-//     private String gender;
-//     private LocalDate dob;
-
-//     public String getName() {
-//         return name;
-//     }
-
-//     public void setName(String name) {
-//         this.name = name;
-//     }
-
-//     public String getGender() {
-//         return gender;
-//     }
-
-//     public void setGender(String gender) {
-//         this.gender = gender;
-//     }
-
-//     public LocalDate getDob() {
-//         return dob;
-//     }
-
-//     public void setDob(LocalDate dob) {
-//         this.dob = dob;
-//     }
-
-//     public int getAge() {
-//         return LocalDate.now().getYear() - dob.getYear();
-//     }
-
-// }
 
 package com.example.clinicbooking.model;
 
@@ -70,3 +31,4 @@ public class UserDetails {
         this.dob = dob;
     }
 }
+
