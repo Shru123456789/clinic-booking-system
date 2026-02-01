a99ff021d99f84a04746c69d1a6a25bec0a0a0f8
Shru123456789 <167177204+Shru123456789@users.noreply.github.com>
2026-02-01 10:30:59 +0530

    Update PaymentService.java

--- clinicbooking/src/main/java/com/example/clinicbooking/service/PaymentService.java
+++ clinicbooking/src/main/java/com/example/clinicbooking/service/PaymentService.java
@@ -1,24 +1,3 @@
-// package com.example.clinicbooking.service;
-
-// import com.example.clinicbooking.model.UserDetails;
-// import org.springframework.stereotype.Service;
-// import java.util.Random;
-
-// @Service
-// public class PaymentService {
-
-//     public void processPayment(UserDetails user, double amount) throws Exception {
-//         System.out.println("[PAYMENT] Processing payment for " + user.getName() + ", Amount: " + amount);
-
-//         // Simulate random payment failure
-//         Random rand = new Random();
-//         if (rand.nextInt(10) < 2) {
-//             throw new Exception("Payment gateway error");
-//         }
-
-//         System.out.println("[PAYMENT] Payment successful");
-//     }
-// }
 
 package com.example.clinicbooking.service;
 
@@ -33,3 +12,4 @@ public class PaymentService {
         return true; // payment success
     }
 }
+
